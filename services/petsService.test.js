7c42197d6c290caf3783b205b743474f30f0fe4f
Chrishaun Jones <ckjones1@student.fullsail.edu>
2024-03-25 12:29:22 -0400

    installed supertest

--- services/petsService.test.js
+++
@@ -1,8 +0,0 @@
-const { petsService, petsServiceById } = require("./petsService");
-
-describe("Pets Service ", () => {
-  test("As a user I should return 200 pets", async () => {
-    const result = await petsService();
-    expect(result.data).toHaveLength(200);
-  });
-});
---
+++ tests/pets.test.js
@@ -0,0 +1,5 @@
+describe("Pets test ", () => {
+  it("My Pets Test", () => {
+    expect(false).toEqual(false);
+  });
+});
